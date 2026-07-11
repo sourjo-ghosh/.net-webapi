29fbcb2766c44aa12d57cda7b902681d0f96ca94
sourjo-ghosh <joyghosh211@outlook.com>
2026-07-11 09:50:49 +0600

    Update .gitignore and modify Program.cs and e-commerce-webapi.http for initial setup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
+
+app.MapGet("/", () =>
+{
+    return "Hello world";
+});
+
+app.MapGet("/hello", () =>
+{
+    return "hi this is joy ghosh sourjoo and i am learning c#/dotnet/webapi thank you";
+});
+
 app.Run();
